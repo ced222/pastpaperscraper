db15ed49ed9aeba65c9db1c3c5e2aa334758fed9
ced222 <79581884+ced222@users.noreply.github.com>
2024-02-15 23:12:13 +0700

    a

--- textanalysis.py
+++ textanalysis.py
@@ -1,58 +1,70 @@
+import spacy
 import os, json
-import nltk
-from nltk.corpus import stopwords, wordnet
-import matplotlib.pyplot as plt
+#cleans the inputted textbook data - tbh i have no idea how the code works i copy pasted it
+nlp = spacy.load("en_core_web_sm")
 cwd = os.getcwd()
-#initalization
-nltk.download('wordnet')
-nltk.download('omw-1.4')
-nltk.download('stopwords')
-stop_words = set(stopwords.words('english'))
-def has_alpha_key(key):
-    if isinstance(key, str) and key.isalpha():
-        return True
-    for i in key:
-        if i.isalpha():
-            return True
-    return False
-
-for number in range(8):
+def openFile():
+    all_topic_texts = []
     #importing the text file to process
     print("Current working directory:", cwd)
-    file_path = '\\topics\\Computer Science\\ch{}comsci.txt'.format(str(number + 1))
+    for i in range(8):
+        file_path = '\\topics\\Computer Science\\ch{}comsci.txt'.format(str(i + 1))
 
-    file_path = cwd + file_path
-    print(file_path)
-    with open(file_path, 'r', encoding='utf-8') as file:
-        file_contents = file.read()
-    words = {} #dictionary to store words
-    #tokenize the text into words
-    file_contents = file_contents.split() 
+        file_path = cwd + file_path
+        print(file_path)
+        with open(file_path, 'r', encoding='utf-8') as file:
+            file_contents = file.read()
+            all_topic_texts.append([file_contents, "ch{}comsci".format(str(i + 1))])
+    return all_topic_texts
 
-    #filter out stopwords
-    file_contents_filtered = [word for word in file_contents if (word.lower() not in stop_words and wordnet.synsets(word))]
+def cleanText(complete_text, textTopic): #function for effectively processing text
+    complete_doc = nlp(complete_text)
+    print(complete_doc)
+    blacklist = {'processes','program','purpose','Describe','candidates', 'explain','including', 'show', 'understanding', 'use', 'justify', 'notes', 'guidance', 'terms'}
+    def is_token_allowed(token):
+        return bool(
+            token
+            and str(token).strip()
+            and not token.is_stop
+            and not token.is_punct
+            and not token.text.lower() in blacklist
+        )
+
+    def preprocess_token(token):
+        
+        return token.lemma_.strip().lower()
+    
+
+        
+    complete_filtered_tokens = [
+        preprocess_token(token)
+        for token in complete_doc
+        if is_token_allowed(token)
+    ]
+    return [complete_filtered_tokens, textTopic]
+
+    
+#initalization
+complete_texts = openFile() #returns all 8 chapters of comsci AS
+complete_cleaned_texts = [] #texts after they have been cleaned
+for text in complete_texts:
+    complete_cleaned_texts.append(cleanText(text[0], text[1]))
+print(complete_cleaned_texts)
 
 
-    for i in file_contents_filtered: #iterates through the entire text and makes key-value pairs - key is the word and value is the number of occurances
-        if i in words:
-            words[i] += 1 
-        else:
-            words[i] = 1
+def checkSentenceMatch(sentence, topicText):
+    totalScore = 0
+    complete_sentence, _ = cleanText(sentence, None)
+    for i in complete_sentence:
+        totalScore += topicText.count(i)
+    return totalScore
 
-    wordstoDelete = [] #make a list of all invalid keys to delete
-    for key, value in words.items():
-        if len(key) == 1 or not has_alpha_key(key):
-            wordstoDelete.append(key)
-    for key in wordstoDelete:
-        del words[key] #delete all invalid keys
-    print(words)
+#mainloop
+while True: 
+    sentence = input("Sentence to match: ")
+    totalMatches = {}
+    for texts in complete_cleaned_texts:
+        totalMatches[texts[1]] = checkSentenceMatch(sentence, texts[0])
 
-    file_path_write = os.path.dirname(file_path) + "\\" + "processed chapters\\ch{}.json".format(str(number + 1))  #write a csv file to this file location
-    with open(file_path_write, 'w+') as file_json:
-        json.dump(words, file_json)
-
-
-
-
-
-
+    key_with_largest_value = max(totalMatches, key=totalMatches.get)
+    print("Most likely topic match: " + key_with_largest_value)
